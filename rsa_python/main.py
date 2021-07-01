5b23ce95ed842957dae12cf10a98fa4dc3935147
oz-0820 <85290191+oz-0820@users.noreply.github.com>
2021-07-01 09:33:54 +0900

    [Fix] ASCIIコードの制御文字の数を間違えていたらしいので修正

--- rsa_python/main.py
+++ rsa_python/main.py
@@ -1,5 +1,5 @@
 import utils
-# ASCIIコードの0x20から0x7Eの合計96文字を利用
+# ASCIIコードの0x20から0x7Eの合計94文字を利用
 
 
 def main():
@@ -44,7 +44,7 @@ def mode2(n, e):
     else:
         print(F'n: {n}\ne: {e}')
 
-    char_plain = input(F'暗号化します。\nASCIIコードの0x20から0x7Eまでの入力に対応しています。(計96文字)\nつまりは半角英数と記号\ntext: ')
+    char_plain = input(F'暗号化します。\nASCIIコードの0x20から0x7Eまでの入力に対応しています。(計94文字)\nつまりは半角英数と記号\ntext: ')
     int_plain = utils.char_to_int(char_plain)
 
     int_crypt = utils.bin_expansion(int_plain, e, n)
--- rsa_python/utils.py
+++ rsa_python/utils.py
@@ -1,61 +1,4 @@
-import copy
-
 import sympy
-import random
-
-
-def make_prime(k_len):
-    tmp_num_s = random.randint(pow(2, k_len-1), pow(2, k_len))
-    tmp_num = copy.copy(tmp_num_s)
-    if tmp_num % 2 == 0:
-        tmp_num += 1
-    t = 0
-    tmp_num_key = [0] * 10
-    while not sympy.isprime(tmp_num):
-        if tmp_num < pow(2, k_len-1) or tmp_num > pow(2, k_len):
-            make_prime(k_len)
-
-        if tmp_num - tmp_num_s <= 100:
-            tmp_num_key = make_tmp_num_key(tmp_num, tmp_num_key)
-            tmp_num += 2
-        else:
-            tmp_num = add_tmp_num(tmp_num, tmp_num_key)
-
-        t += 1
-        print(F't:{t}\ntmp: {tmp_num - tmp_num_s}\n\n')
-    return tmp_num
-
-
-def add_tmp_num(tmp_num, tmp_num_key):
-    try:
-        if (tmp_num - tmp_num_key[0]) % 3 == 0:
-            make_raise()
-        elif (tmp_num - tmp_num_key[1]) % 5 == 0:
-            make_raise()
-        elif (tmp_num - tmp_num_key[2]) % 7 == 0:
-            make_raise()
-        elif (tmp_num - tmp_num_key[3]) % 11 == 0:
-            make_raise()
-        else:
-            return tmp_num
-    except Exception:
-        return add_tmp_num(tmp_num + 2, tmp_num_key)
-
-
-def make_raise():
-    raise Exception
-
-
-def make_tmp_num_key(tmp_num, tmp_num_key):
-    if tmp_num % 3 == 0:
-        tmp_num_key[0] = copy.copy(tmp_num)
-    elif tmp_num % 5 == 0:
-        tmp_num_key[1] = copy.copy(tmp_num)
-    elif tmp_num % 7 == 0:
-        tmp_num_key[2] = copy.copy(tmp_num)
-    elif tmp_num % 11 == 0:
-        tmp_num_key[3] = copy.copy(tmp_num)
-    return tmp_num_key
 
 
 def make_keys(key_len, e):  # return n, e, lcm, d
@@ -105,26 +48,26 @@ def char_to_int(data):  # 文字列を突っ込むと数列を返す
     int_list.reverse()
     int_data = 0
     for i in range(len(int_list)):
-        int_data += int_list[i] * pow(95, i)
+        int_data += int_list[i] * pow(94, i)
     return int_data
 
 
 def int_to_char(data):  # 数列を突っ込むと文字列を返す
     i = 0
     while True:
-        if data < pow(95, i+1):
+        if data < pow(94, i+1):
             break
         i += 1
 
     int_list = []
     while i >= 0:
         int_text = 0
-        a = pow(95, i)
-        while int_text < 95:
+        a = pow(94, i)
+        while int_text < 94:
             if (int_text + 1) * a > data:
                 int_list.append(int_text)
                 data -= int_text * a
-                int_text += 95
+                int_text += 94
             int_text += 1
         i -= 1
 
